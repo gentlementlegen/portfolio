a05e47bcafb60136e67226b33f5f086e88131caa
Fernando <fernand.veyrier@epitech.eu>
2022-02-04 01:32:57 +0900

    Moved nav bar to app

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,4 @@
 import React, { PropsWithChildren } from 'react'
-import NavBar from 'components/layout/NavBar'
-import Footer from 'components/layout/Footer'
 import styles from 'styles/Home.module.css'
 import { Box, BoxProps } from '@mui/material'
 
@@ -8,13 +6,9 @@ const MainLayout = (props: PropsWithChildren<BoxProps>): JSX.Element => {
   const { children, ...rest } = props
 
   return (
-    <>
-      <NavBar />
-      <Box className={styles.mainTitle} {...rest}>
-        {children}
-      </Box>
-      <Footer />
-    </>
+    <Box className={styles.mainTitle} {...rest}>
+      {children}
+    </Box>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,17 @@ import { ThemeProvider } from '@mui/material/styles'
 import theme from 'theme'
 import { ApolloProvider } from '@apollo/client'
 import client from 'apolloClient'
+import NavBar from 'components/layout/NavBar'
+import Footer from 'components/layout/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <ApolloProvider client={client}>
         <CssBaseline />
+        <NavBar />
         <Component {...pageProps} />
+        <Footer />
       </ApolloProvider>
     </ThemeProvider>
   )
