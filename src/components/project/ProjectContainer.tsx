7963647389e51ecfd0a8d15f6a2ba527356392db
Fernando <fernand.veyrier@epitech.eu>
2022-02-08 00:21:45 +0900

    Linked query for projects

--- src/components/project/ProjectContainer.tsx
+++ src/components/project/ProjectContainer.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import { Box, Grid } from '@mui/material'
 import ProjectCard from './ProjectCard'
+import { gql, useQuery } from '@apollo/client'
+
+const QUERY = gql`
+  query Projects {
+    allProjects {
+      id
+      title
+    }
+  }
+`
 
 const ProjectContainer = (): JSX.Element => {
+  const { data } = useQuery(QUERY)
+
   return (
     <Box
       sx={{
@@ -10,21 +22,11 @@ const ProjectContainer = (): JSX.Element => {
       }}
     >
       <Grid container justifyContent={'center'} id={'projects'} spacing={4}>
-        <Grid item sm={4} xs={6}>
-          <ProjectCard title={'project 1'} />
-        </Grid>
-        <Grid item sm={4} xs={6}>
-          <ProjectCard title={'project 1'} />
-        </Grid>
-        <Grid item sm={4} xs={6}>
-          <ProjectCard title={'project 1'} />
-        </Grid>
-        <Grid item sm={4} xs={6}>
-          <ProjectCard title={'project 1'} />
-        </Grid>
-        <Grid item sm={4} xs={6}>
-          <ProjectCard title={'project 1'} />
-        </Grid>
+        {data?.allProjects?.map((project) => (
+          <Grid item sm={4} xs={6} key={project.id}>
+            <ProjectCard title={project.title} />
+          </Grid>
+        ))}
       </Grid>
     </Box>
   )
