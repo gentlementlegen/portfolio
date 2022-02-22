4d6cf0b8af44cc8e4973d632a2c6f2b1f36184fb
Fernando <fernand.veyrier@epitech.eu>
2022-02-23 02:43:28 +0900

    test without static props

--- src/pages/games/[id].tsx
+++ src/pages/games/[id].tsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import { NextPage } from 'next'
 import { gql } from '@apollo/client'
-import client from 'apolloClient'
-import { Typography } from '@mui/material'
-import { Game } from 'lib/models/Game'
 
 const QUERY_PROJECT = gql`
   query Project($id: ID!) {
@@ -23,31 +20,31 @@ const QUERY_PROJECTS = gql`
   }
 `
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const { data } = await client.query({ query: QUERY_PROJECTS })
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const { data } = await client.query({ query: QUERY_PROJECTS })
+//
+//   return {
+//     paths: data.projects.map((o) => ({ params: { id: o.id } })),
+//     fallback: false,
+//   }
+// }
+//
+// export const getStaticProps: GetStaticProps<Game> = async ({ params }) => {
+//   const { data } = await client.query<{ project: Game }>({ query: QUERY_PROJECT, variables: { id: params.id } })
+//
+//   return {
+//     props: {
+//       ...data.project,
+//     },
+//   }
+// }
 
-  return {
-    paths: data.projects.map((o) => ({ params: { id: o.id } })),
-    fallback: false,
-  }
-}
-
-export const getStaticProps: GetStaticProps<Game> = async ({ params }) => {
-  const { data } = await client.query<{ project: Game }>({ query: QUERY_PROJECT, variables: { id: params.id } })
-
-  return {
-    props: {
-      ...data.project,
-    },
-  }
-}
-
-const GamePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (props) => {
-  const { title, description } = props
+const GamePage: NextPage = (props) => {
+  // const { title, description } = props
   return (
     <div>
-      <h1>{title}</h1>
-      <Typography>{description}</Typography>
+      {/*<h1>{title}</h1>*/}
+      {/*<Typography>{description}</Typography>*/}
     </div>
   )
 }
