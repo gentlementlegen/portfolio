65962f158fa09d233941786519c88111b648d0bb
Fernando <fernand.veyrier@epitech.eu>
2022-02-23 01:51:21 +0900

    Fixed project page

--- src/pages/games/[id].tsx
+++ src/pages/games/[id].tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 import { gql } from '@apollo/client'
 import client from 'apolloClient'
+import { Typography } from '@mui/material'
+import { Game } from 'lib/models/Game'
 
 const QUERY_PROJECT = gql`
   query Project($id: ID!) {
     project: Project(id: $id) {
       id
       title
+      description
     }
   }
 `
@@ -24,24 +27,29 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const { data } = await client.query({ query: QUERY_PROJECTS })
 
   return {
-    paths: data.projects.map((o) => ({ params: o.id })),
+    paths: data.projects.map((o) => ({ params: { id: o.id } })),
     fallback: false,
   }
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { data } = await client.query({ query: QUERY_PROJECT, variables: { id: params.id } })
+export const getStaticProps: GetStaticProps<Game> = async ({ params }) => {
+  const { data } = await client.query<{ project: Game }>({ query: QUERY_PROJECT, variables: { id: params.id } })
 
   return {
     props: {
-      ...data,
+      ...data.project,
     },
   }
 }
 
 const GamePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (props) => {
-  console.log(props)
-  return <div>This is a game</div>
+  const { title, description } = props
+  return (
+    <div>
+      <h1>{title}</h1>
+      <Typography>{description}</Typography>
+    </div>
+  )
 }
 
 export default GamePage
