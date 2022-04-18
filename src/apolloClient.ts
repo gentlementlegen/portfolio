4df1132d1434a6868b60731532b0aef1fec6f857
Fernando <fernand.veyrier@epitech.eu>
2022-04-19 01:16:54 +0900

    Changed upload url

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { createUploadLink } from 'apollo-upload-client'
 
 const link = createUploadLink({
-  uri: `${process.env.VERCEL_URL}/api/graphql`,
+  uri: '/api/graphql',
 })
 
 const client = new ApolloClient({
