526dca1aaa7b1b4506c38cea9eaddc7e0bbd94ab
Fernando <fernand.veyrier@epitech.eu>
2022-04-19 21:06:40 +0900

    Fixed image api route

--- src/pages/api/image/[id].ts
+++ src/pages/api/image/[id].ts
@@ -3,7 +3,6 @@ import mongoose from 'mongoose'
 import { getGfs } from 'lib/dbConnect'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  return res.status(200).json(req.query.id)
   return getGfs()
     .then(async (gfs) => {
       const readStream = gfs.openDownloadStream(new mongoose.Types.ObjectId(req.query.id as string))
