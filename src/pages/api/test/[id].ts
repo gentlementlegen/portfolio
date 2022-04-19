b401e3a16d480ea3c001aeb55f44a62f2ae7baa0
Fernando <fernand.veyrier@epitech.eu>
2022-04-19 09:23:49 +0900

    Test api route

--- src/pages/api/test/[id].ts
+++ src/pages/api/test/[id].ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  return res.end(req.query.id)
+  res.end(req.query.id)
 }
 
 export default handler
