ce7616a8bf15d692ec1c223fa93ea64bd03050e1
Fernando <fernand.veyrier@epitech.eu>
2022-04-21 20:59:17 +0900

    Removed admin from sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -4,4 +4,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL ? `https://${process.env.SITE_URL}` : 'https://www.fernand-veyrier.xyz',
   generateRobotsTxt: true,
   // ...other options
+  exclude: ['/admin'],
 }
