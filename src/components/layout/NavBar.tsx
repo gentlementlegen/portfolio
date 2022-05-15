dc8872e98a6283aecf44b01073fae59968cfd1d7
Fernando <fernand.veyrier@epitech.eu>
2022-05-15 14:49:59 +0900

    Fixed anchor navigation when coming from other pages

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import { AppBar, Button, Link as MuiLink, Toolbar, Typography } from '@mui/material'
+import React, { PropsWithChildren } from 'react'
+import { AppBar, Button, Link as MuiLink, Switch, Toolbar, Typography } from '@mui/material'
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from 'styles/Home.module.css'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import AnchorLink, { AnchorLinkProps } from 'react-anchor-link-smooth-scroll'
+import { ColorModeContext } from 'components/context/ColorModeContext'
+import { useRouter } from 'next/router'
+
+const LinkElement = ({ children, href, offset }: PropsWithChildren<AnchorLinkProps>) => {
+  const { route } = useRouter()
+
+  return route !== '/' ? (
+    <Link href={`/${href}`} passHref>
+      {children}
+    </Link>
+  ) : (
+    <AnchorLink href={href} offset={offset}>
+      {children}
+    </AnchorLink>
+  )
+}
 
 const NavBar = (): JSX.Element => {
+  const { toggleColorMode } = React.useContext(ColorModeContext)
+
+  const handleSwitchChange = () => {
+    toggleColorMode()
+  }
+
   return (
     <AppBar position={'sticky'} sx={{ backgroundImage: 'none' }}>
       <Head>
@@ -15,19 +37,20 @@ const NavBar = (): JSX.Element => {
         <Link href={'/'} passHref>
           <MuiLink className={styles.navBarTitle} color={'secondary'} underline={'none'}>
             <Typography variant={'h6'} component={'div'} className={styles.navBarTitle}>
-              Portfolio
+              Fernand Veyrier
             </Typography>
           </MuiLink>
         </Link>
-        <AnchorLink href={'#home'} offset={100}>
+        <LinkElement href={'#home'} offset={100}>
           <Button color={'inherit'}>Home</Button>
-        </AnchorLink>
-        <AnchorLink href={'#about'} offset={100}>
+        </LinkElement>
+        <LinkElement href={'#about'} offset={100}>
           <Button color={'inherit'}>About</Button>
-        </AnchorLink>
-        <AnchorLink href={'#contact'} offset={100}>
+        </LinkElement>
+        <LinkElement href={'#contact'} offset={100}>
           <Button color={'inherit'}>Contact</Button>
-        </AnchorLink>
+        </LinkElement>
+        <Switch size={'small'} color={'secondary'} onChange={handleSwitchChange} />
       </Toolbar>
     </AppBar>
   )
