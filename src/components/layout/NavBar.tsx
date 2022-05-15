f877304f2cde39f9806b3a2fc4b92e61b3778cc1
Fernando <fernand.veyrier@epitech.eu>
2022-05-15 15:24:11 +0900

    Added moon and sun icons to the switch

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren } from 'react'
-import { AppBar, Button, Link as MuiLink, Switch, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, Link as MuiLink, styled, Switch, SwitchProps, Toolbar, Typography } from '@mui/material'
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from 'styles/Home.module.css'
@@ -20,6 +20,69 @@ const LinkElement = ({ children, href, offset }: PropsWithChildren<AnchorLinkPro
     </AnchorLink>
   )
 }
+
+const CustomSwitch = styled((props: SwitchProps) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+      '& .MuiSwitch-thumb:before': {
+        backgroundImage: 'url(/moon.svg)',
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color: theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[600],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 22,
+    height: 22,
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      padding: 4,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: 'url(/sun.svg)',
+      backgroundOrigin: 'content-box',
+    },
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}))
 
 const NavBar = (): JSX.Element => {
   const { toggleColorMode } = React.useContext(ColorModeContext)
@@ -50,7 +113,7 @@ const NavBar = (): JSX.Element => {
         <LinkElement href={'#contact'} offset={100}>
           <Button color={'inherit'}>Contact</Button>
         </LinkElement>
-        <Switch size={'small'} color={'secondary'} onChange={handleSwitchChange} />
+        <CustomSwitch size={'small'} color={'secondary'} onChange={handleSwitchChange} />
       </Toolbar>
     </AppBar>
   )
