2b3cd30645f9399c5098cdbc3f4ba4ae24efedbf
Fernando <fernand.veyrier@epitech.eu>
2022-06-06 11:55:22 +0900

    Fixed translation inside dynamic routing paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,8 @@ import AboutSection from 'components/about/AboutSection'
 import ContactForm from 'components/contact/ContactForm'
 import { Trans, useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { SxProps } from '@mui/system'
+import { Theme } from '@mui/material/styles'
 
 export const getStaticProps: GetStaticProps<{ projects: Project[]; skills: Skill[] }> = async ({ locale }) => {
   await dbConnect()
@@ -31,6 +33,45 @@ export const getStaticProps: GetStaticProps<{ projects: Project[]; skills: Skill
   }
 }
 
+const style: { [k: string]: SxProps<Theme> } = {
+  root: (theme) => ({
+    height: 'calc(100vh - 64px)',
+    top: 64,
+    [theme.breakpoints.down('sm')]: {
+      height: 'calc(100vh - 56px)',
+      top: 56,
+    },
+    width: '100%',
+    position: 'absolute',
+    zIndex: -2,
+    backgroundColor: 'black',
+    opacity: 0.7,
+  }),
+  projectContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'fixed',
+    left: '50%',
+    bottom: 8,
+    transform: 'translateX(-50%)',
+  },
+  videoContainer: (theme) => ({
+    height: 'calc(100vh - 64px)',
+    top: 64,
+    [theme.breakpoints.down('sm')]: {
+      height: 'calc(100vh - 56px)',
+      top: 56,
+    },
+    width: '100%',
+    position: 'fixed',
+    zIndex: -3,
+    objectFit: 'cover',
+    filter: 'blur(4px)',
+    msFilter: 'blur(4px)',
+    webkitFilter: 'blur(4px)',
+  }),
+}
+
 export default function Home(props: InferGetStaticPropsType<typeof getStaticProps>) {
   const { projects, skills } = props
   const { t } = useTranslation('common')
@@ -38,45 +79,8 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
 
   return (
     <MainLayout>
-      <Box
-        id={'home'}
-        sx={{
-          height: 'calc(100vh - 64px)',
-          top: 64,
-          [theme.breakpoints.down('sm')]: {
-            height: 'calc(100vh - 56px)',
-            top: 56,
-          },
-          width: '100%',
-          position: 'absolute',
-          zIndex: -2,
-          backgroundColor: 'black',
-          opacity: 0.7,
-        }}
-      />
-      <Box
-        component={'video'}
-        autoPlay
-        muted
-        loop
-        preload="none"
-        id="backgroundVideo"
-        sx={{
-          height: 'calc(100vh - 64px)',
-          top: 64,
-          [theme.breakpoints.down('sm')]: {
-            height: 'calc(100vh - 56px)',
-            top: 56,
-          },
-          width: '100%',
-          position: 'fixed',
-          zIndex: -3,
-          objectFit: 'cover',
-          filter: 'blur(4px)',
-          msFilter: 'blur(4px)',
-          webkitFilter: 'blur(4px)',
-        }}
-      >
+      <Box id={'home'} sx={style.root} />
+      <Box component={'video'} autoPlay muted loop preload="none" id="backgroundVideo" sx={style.videoContainer}>
         <source src="backgroundVideo.mp4" type="video/mp4" />
       </Box>
       <Container className={styles.mainGrid}>
@@ -94,16 +98,7 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
           </Grid>
         </Grid>
       </Container>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          position: 'fixed',
-          left: '50%',
-          bottom: 8,
-          transform: 'translateX(-50%)',
-        }}
-      >
+      <Box sx={style.projectContainer}>
         <AnchorLink href={'#projects'} offset={100}>
           <IconButton size={'large'} color={'secondary'}>
             <KeyboardArrowDownRoundedIcon />
