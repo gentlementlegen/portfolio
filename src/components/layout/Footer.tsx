416affcc982fb66197246b27a32988a3dcd24e83
Fernando <fernand.veyrier@epitech.eu>
2022-06-06 11:05:21 +0900

    Added main and footer wrapper sections

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,22 +3,24 @@ import { Paper, Typography } from '@mui/material'
 
 const Footer = (): JSX.Element => {
   return (
-    <Paper
-      square
-      elevation={0}
-      sx={{
-        padding: 2,
-        position: 'relative',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        columnGap: 1,
-      }}
-    >
-      <Typography variant={'caption'} align={'center'} component={'p'}>
-        Games are love, games are life | Copyright Fernand Veyrier, {new Date().getFullYear()}
-      </Typography>
-    </Paper>
+    <footer>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          padding: 2,
+          position: 'relative',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          columnGap: 1,
+        }}
+      >
+        <Typography variant={'caption'} align={'center'} component={'p'}>
+          Games are love, games are life | Copyright Fernand Veyrier, {new Date().getFullYear()}
+        </Typography>
+      </Paper>
+    </footer>
   )
 }
 
