4f3aac5b3f120ecf946c7f1f161b87a3813d9676
Fernando <fernand.veyrier@epitech.eu>
2022-11-17 17:55:18 +0900

    Test locale path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr', 'ko'],
-    // localePath: path.resolve('./public/locales'),
   },
+  localePath: path.resolve('./public/locales'),
 }
