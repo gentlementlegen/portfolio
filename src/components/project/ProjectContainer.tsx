fa7eedfd8dc99a0a0eb9f0c3f0dc969dfed4cc3e
Fernando <fernand.veyrier@epitech.eu>
2022-11-17 15:13:25 +0900

    Changed home page with Hygraph API

--- src/components/project/ProjectContainer.tsx
+++ src/components/project/ProjectContainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, Grid } from '@mui/material'
-import { Project } from 'lib/models/Project'
 import ProjectCard from 'components/project/ProjectCard'
+import { Project } from 'generated/graphql'
 
 const ProjectContainer = (props: { projects: Project[] }): JSX.Element => {
   const { projects } = props
