cf17ee724c065ae4c9a20f6ee19e02d9d790b877
Fernando <fernand.veyrier@epitech.eu>
2022-11-19 03:56:10 +0900

    Added nodemailer

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,6 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from 'nodemailer'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body)
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { name, email, message } = req.body
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  })
+  const info = await transporter.sendMail({
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    subject: `New message from ${name}`,
+    text: `From: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
+  })
+  res.status(info.accepted ? 200 : 500).json(info.response)
 }
