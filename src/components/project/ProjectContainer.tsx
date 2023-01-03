29a999c73fe7b2ce3907f1c34a05f9edbb04b2df
Fernando <fernand.veyrier@epitech.eu>
2023-01-03 02:26:41 -0800

    Added framer-motion for projects

--- src/components/project/ProjectContainer.tsx
+++ src/components/project/ProjectContainer.tsx
@@ -2,9 +2,31 @@ import React from 'react'
 import { Box, BoxProps, Grid } from '@mui/material'
 import ProjectCard from 'components/project/ProjectCard'
 import { Project } from 'generated/graphql'
+import { motion, Variants } from 'framer-motion'
 
 interface ProjectContainerProps extends BoxProps {
   projects: Project[]
+}
+
+const cardVariant: Variants = {
+  hidden: {
+    opacity: 0,
+    transform: 'translateY(50px)',
+  },
+  visible: {
+    opacity: 1,
+    transform: 'translateY(0px)',
+  },
+}
+
+const container: Variants = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.06,
+      delayChildren: 0.03,
+    },
+  },
 }
 
 const ProjectContainer = (props: ProjectContainerProps): JSX.Element => {
@@ -21,13 +43,17 @@ const ProjectContainer = (props: ProjectContainerProps): JSX.Element => {
       ]}
       {...rest}
     >
-      <Grid container justifyContent={'center'} id={'projects'} spacing={2}>
-        {projects?.map((project) => (
-          <Grid item sm={3} xs={6} key={project.id}>
-            <ProjectCard project={project} />
-          </Grid>
-        ))}
-      </Grid>
+      <motion.div variants={container} initial="hidden" whileInView="visible" viewport={{ once: true }}>
+        <Grid container justifyContent={'center'} id={'projects'} spacing={2}>
+          {projects?.map((project) => (
+            <Grid item sm={3} xs={6} key={project.id}>
+              <motion.div variants={cardVariant}>
+                <ProjectCard project={project} />
+              </motion.div>
+            </Grid>
+          ))}
+        </Grid>
+      </motion.div>
     </Box>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -108,7 +108,7 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
             <motion.div
               initial={{ opacity: 0, translateY: 16 }}
               animate={{ opacity: 1, translateY: 0 }}
-              transition={{ duration: 0.3, ease: 'easeInOut' }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
             >
               <Typography variant={'h1'} component={'h2'} align={'center'} gutterBottom color={'#ffffff'}>
                 {t('h1')}
@@ -117,7 +117,7 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
             <motion.div
               initial={{ opacity: 0, translateY: 16 }}
               animate={{ opacity: 1, translateY: 0 }}
-              transition={{ duration: 0.3, delay: 0.15, ease: 'easeInOut' }}
+              transition={{ duration: 0.4, delay: 0.1, ease: 'easeInOut' }}
             >
               <Typography variant={'h5'} component={'h1'} align={'center'} color={'#ffffff'}>
                 <Trans i18nKey={'h2'}>
