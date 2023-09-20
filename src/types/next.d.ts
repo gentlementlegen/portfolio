3b8dea5757f48ab71bbaac7c717f048ac4b4953b
Fernando <fernand.veyrier@epitech.eu>
2023-09-20 15:02:13 +0900

    Fixed extended typing

--- src/types/next.d.ts
+++ src/types/next.d.ts
@@ -1,14 +1,14 @@
+import { NextComponentType, NextPageContext } from 'next'
+import { AppPropsType } from 'next/dist/shared/lib/utils'
+import { Router } from 'next/router'
+
 declare module 'next/app' {
-  import { NextComponentType, NextPageContext } from 'next'
-  import { AppPropsType } from 'next/dist/shared/lib/utils'
-  import { Router } from 'next/router'
   export declare type AppProps<P = any> = AppPropsType<Router, P> & {
     Component: NextComponentType<NextPageContext, any, any> & { hideMainLayout?: boolean }
   }
 }
 
 declare module 'next' {
-  import { NextComponentType, NextPageContext } from 'next'
   export declare type NextPage<P = {}, IP = P> = NextComponentType<NextPageContext, IP, P> & {
     hideMainLayout?: boolean
   }
